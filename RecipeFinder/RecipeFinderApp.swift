f982e4499b2450ddffe37fd240e933706fb0c206
gabi brown <gbrown@lcs.on.ca>
2022-05-06 11:53:08 -0400

    Initial Commit

---
+++ RecipeFinder/RecipeFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeFinderApp.swift
+//  RecipeFinder
+//
+//  Created by gabi brown on 2022-05-06.
+//
+
+import SwiftUI
+
+@main
+struct RecipeFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

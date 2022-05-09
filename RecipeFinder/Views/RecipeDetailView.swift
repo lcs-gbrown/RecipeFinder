4ec049b550479211d808617dd0b47308353b7ab1
gabi brown <gbrown@lcs.on.ca>
2022-05-09 15:25:49 -0400

    passed in recipe properties to my recipe view

--- RecipeFinder/Views/RecipeDetailView.swift
+++ RecipeFinder/Views/RecipeDetailView.swift
@@ -8,26 +8,43 @@
 import SwiftUI
 
 struct RecipeDetailView: View {
+    // MARK: Stored properties
     
+    var recipe: Recipe
+    
+    // MARK: Computed properties
     var body: some View {
         
-        NavigationView {
+        ScrollView {
             
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading)  {
                 
-//                AsyncImage(url: URL(string: "https://www.themealdb.com/images/media/meal//sbx7n71587673021.jpg"))
+                Text(recipe.strMeal)
+                    .font(.largeTitle)
                 
-                VideoView(videoID: "https://www.youtube.com/watch?v=1WDesu7bUDM")
+                VideoView(strYouTube: "https://www.youtube.com/watch?v=1WDesu7bUDM")
                 
-                Text("Wrap the chicken loosely between plastic wrap and pound gently with the flat side of a meat tenderizer until about 1/2 inch thick all around.Cut into two pieces, as even as possible. Marinate in the pickle juice for 30 minutes to one hour (add a teaspoon of Tabasco sauce now for a spicy sandwich).Beat the egg with the milk in a bowl.Combine the flour, sugar, and spices in another bowl.Dip the chicken pieces each into the egg on both sides, then coat in flour on both sides.Heat the oil in a skillet (1/2 inch deep) to about 345-350.Fry each cutlet for 2 minutes on each side, or until golden and cooked through.Blot on paper and serve on toasted buns with pickle slices.")
+                RemoteImageView(fromURL: URL(string: recipe.strMealThumb)!)
+                
+                Text(recipe.strInstructions)
+                    .font(.body)
+                
+                Link("View recipe", destination: URL(string: recipe.strSource)!)
+                    .padding(.top, 5)
+               
+                Link("View video", destination: URL(string: recipe.strYouTube)!)
+                    .padding(.top, 5)
             }
-            .navigationTitle("Chicken Breast")
+            
+            
         }
+      
     }
 }
 
+
 struct RecipeDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeDetailView()
+        RecipeDetailView(recipe: testRecipe)
     }
 }
